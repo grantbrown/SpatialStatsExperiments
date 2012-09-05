ea76a8272c5d1cb2178d38072984020312b7f810
Grant Brown <grant.brown73@gmail.com>
2012-09-04 21:55:12 -0500

    fix some bugs

--- variogram.py
+++ variogram.py
@@ -18,9 +18,10 @@ class Variogram():
         grid = np.zeros((n**2)*3)
         grid.shape = (n**2,3)
         idx = 0
-        x = lb
-        y = lb
+        x = lb + granularity
+        y = lb + granularity
         for i in xrange(int(n/2)):
+            y = lb
             for j in xrange(int(n/2)):
                 val = self.f(norm([x,y]))
                 grid[idx,:] = (x,y,val)
@@ -30,19 +31,19 @@ class Variogram():
                 grid[idx,:] = (x,-y,val)
                 idx += 1
                 grid[idx,:] = (-x,-y,val)
-                idx += 1
-                x += granularity
+                idx += 1 
                 y += granularity
+            x += granularity
         return(grid)
 
-    def write_las_grid(self,filename, lb = 100, ub = 100, granularity = 0.1):
+    def write_las_grid(self,filename, lb = -10, ub = 10, granularity = 0.1):
         out_file = File(filename, mode = "w", header = Header())
-        out_file.scale = (1.0,1.0,1.0)
+        out_file.header.scale = (0.001,0.001,0.001)
         grid = self.get_3d_grid(lb, ub, granularity)
-        out_file.X = grid[:,0]
-        out_file.Y = grid[:,1]
-        out_file.Z = grid[:,2]
-        out_file.intensity = grid[:,2]
+        out_file.x = grid[:,0]* 100
+        out_file.y = grid[:,1] * 100
+        out_file.z = grid[:,2] * 100
+        out_file.intensity = grid[:,2] * 100
         return(out_file)
 
 
@@ -67,7 +68,7 @@ class SphericalVariogram(Variogram):
     def f(self, t):
         if (t <= 0 or t > (1/self.phi)):
             return(0)
-        return(self.tau_squared + self.simga_squared*((3/2.0)*self.phi*t - 0.5*(self.phi*t)**3))
+        return(self.tau_squared + self.sigma_squared*((3/2.0)*self.phi*t - 0.5*(self.phi*t)**3))
 
 
 class ExponentialVariogram(Variogram):
@@ -106,9 +107,27 @@ class GaussianVariogram(Variogram):
             return(0)
         return(self.tau_squared + self.sigma_squared*(1-np.exp(-1*(self.phi**2)*(t**2))))
 
-
-
-
+if __name__ == "__main__":
+    print("Writing Gaussian Variogram")
+    gauss = GaussianVariogram(10,10,10)
+    outfile = gauss.write_las_grid("gaussian.las", -1, 1, 0.01)
+    outfile.close()
+    print("Writing Powered Exponential Variogram")
+    pexp = PoweredExponentialVariogram(10,10,10,1)
+    outfile = pexp.write_las_grid("powered_exp.las", -1, 1, 0.01)
+    outfile = outfile.close()
+    print("Writing Exponential variogram")
+    exp = ExponentialVariogram(10,10,10)
+    outfile = exp.write_las_grid("exp.las", -1, 1, 0.01)
+    outfile.close()
+    print("Writing Spherical Variogram")
+    spherical = SphericalVariogram(10,10,0.5)
+    outfile = spherical.write_las_grid("sphere.las", -1,1,0.01)
+    outfile.close()
+    print("Writing Linear Variogram")
+    linear = LinearVariogram(10,10)
+    outfile = linear.write_las_grid("linear.las", -1,1,0.01)
+    outfile.close()
 
 
 
